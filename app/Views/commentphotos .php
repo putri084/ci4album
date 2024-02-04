4e208b51336a58c32ae252271deff2f2e1fd1715
putri084 <aprilliaputri848@gmail.com>
2024-02-04 15:59:02 +0700

    pembaruan

--- app/Views/commentphotos .php
+++ app/Views/commentphotos .php
@@ -14,9 +14,9 @@
         <thead>
           <tr>
             <th>No</th>
-            <th>Photo id</th>
-<th>User id</th>
-<th>Comment</th>
+            <th>Photo</th>
+            <th>User id</th>
+            <th>Comment</th>
 
             <th>Aksi</th>
           </tr>
@@ -30,48 +30,48 @@
 
 <!-- ADD modal content -->
 <div id="data-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-md">
-        <div class="modal-content">
-            <div class="text-center bg-info p-3" id="model-header">
-                <h4 class="modal-title text-white" id="info-header-modalLabel"></h4>
+  <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-md">
+    <div class="modal-content">
+      <div class="text-center bg-info p-3" id="model-header">
+        <h4 class="modal-title text-white" id="info-header-modalLabel"></h4>
+      </div>
+      <div class="modal-body">
+        <form id="data-form" class="pl-3 pr-3">
+          <?= csrf_field() ?>
+          <div class="row">
+            <input type="hidden" id="id" name="id" class="form-control" placeholder="Id" required>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="form-group mb-3">
+                <label for="photo_id" class="col-form-label"> Photo id: <span class="text-danger">*</span> </label>
+                <input type="number" id="photo_id" name="photo_id" class="form-control" placeholder="Photo id" minlength="0" required>
+              </div>
             </div>
-            <div class="modal-body">
-                <form id="data-form" class="pl-3 pr-3">
-                <?= csrf_field() ?>
-                <div class="row">
-<input type="hidden" id="id" name="id" class="form-control" placeholder="Id" required>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<div class="form-group mb-3">
-									<label for="photo_id" class="col-form-label"> Photo id: <span class="text-danger">*</span> </label>
-									<input type="number" id="photo_id" name="photo_id" class="form-control" placeholder="Photo id" minlength="0"  required>
-								</div>
-							</div>
-							<div class="col-md-12">
-								<div class="form-group mb-3">
-									<label for="user_id" class="col-form-label"> User id: <span class="text-danger">*</span> </label>
-									<input type="number" id="user_id" name="user_id" class="form-control" placeholder="User id" minlength="0"  required>
-								</div>
-							</div>
-							<div class="col-md-12">
-								<div class="form-group mb-3">
-									<label for="comment" class="col-form-label"> Comment: </label>
-									<textarea cols="40" rows="5" id="comment" name="comment" class="form-control" placeholder="Comment" minlength="0"  ></textarea>
-								</div>
-							</div>
-						</div>
+            <div class="col-md-12">
+              <div class="form-group mb-3">
+                <label for="user_id" class="col-form-label"> User id: <span class="text-danger">*</span> </label>
+                <input type="number" id="user_id" name="user_id" class="form-control" placeholder="User id" minlength="0" required>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group mb-3">
+                <label for="comment" class="col-form-label"> Comment: </label>
+                <textarea cols="40" rows="5" id="comment" name="comment" class="form-control" placeholder="Comment" minlength="0"></textarea>
+              </div>
+            </div>
+          </div>
 
-                    <div class="form-group text-center">
-                        <div class="btn-group">
-                            <button type="submit" class="btn btn-success mr-2" id="form-btn"><?= lang("App.save") ?></button>
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal"><?= lang("App.cancel") ?></button>
-                        </div>
-                    </div>
-                </form>
+          <div class="form-group text-center">
+            <div class="btn-group">
+              <button type="submit" class="btn btn-success mr-2" id="form-btn"><?= lang("App.save") ?></button>
+              <button type="button" class="btn btn-danger" data-bs-dismiss="modal"><?= lang("App.cancel") ?></button>
             </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
+          </div>
+        </form>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
 </div>
 <!-- /ADD modal content -->
 
@@ -80,32 +80,32 @@
 <!-- page script -->
 <?= $this->section("script") ?>
 <script>
-    let csrfHash = '<?= csrf_hash(); ?>'
-    let csrfToken = '<?= csrf_token(); ?>'
-    // dataTables
-    $(function() {
-        var table = $('#data_table').removeAttr('width').DataTable({
-            "paging": true,
-            "lengthChange": false,
-            "searching": true,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "scrollY": '45vh',
-            "scrollX": true,
-            "scrollCollapse": false,
-            "responsive": false,
-            "ajax": {
-                "url": '<?php echo base_url($controller . "/getAll") ?>',
-                "type": "POST",
-                "dataType": "json",
-                "data":{
-                    [csrfToken]: csrfHash
-                },
-                async: "true"
-            }
-        });
+  let csrfHash = '<?= csrf_hash(); ?>'
+  let csrfToken = '<?= csrf_token(); ?>'
+  // dataTables
+  $(function() {
+    var table = $('#data_table').removeAttr('width').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": true,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "scrollY": '45vh',
+      "scrollX": true,
+      "scrollCollapse": false,
+      "responsive": false,
+      "ajax": {
+        "url": '<?php echo base_url($controller . "/getAll") ?>',
+        "type": "POST",
+        "dataType": "json",
+        "data": {
+          [csrfToken]: csrfHash
+        },
+        async: "true"
+      }
     });
+  });
 
   var urlController = '';
   var submitText = '';
@@ -122,7 +122,7 @@
     // reset the form 
     $("#data-form")[0].reset();
     $(".form-control").removeClass('is-invalid').removeClass('is-valid');
-    if (typeof id === 'undefined' ||id < 1) { //add
+    if (typeof id === 'undefined' || id < 1) { //add
       urlController = '<?= base_url($controller . "/add") ?>';
       submitText = '<?= lang("App.save") ?>';
       $("#info-header-modalLabel").text('<?= lang("App.add") ?>');
@@ -135,7 +135,7 @@
         url: '<?= base_url($controller . "/getOne") ?>',
         type: 'post',
         data: {
-         id:id,
+          id: id,
           [csrfToken]: csrfHash
         },
         dataType: 'json',
@@ -144,10 +144,10 @@
           $("#form-btn").text(submitText);
           $('#data-modal').modal('show');
           //insert data to form
-         			$("#data-form #id").val(response.id);
-			$("#data-form #photo_id").val(response.photo_id);
-			$("#data-form #user_id").val(response.user_id);
-			$("#data-form #comment").val(response.comment);
+          $("#data-form #id").val(response.id);
+          $("#data-form #photo_id").val(response.photo_id);
+          $("#data-form #user_id").val(response.user_id);
+          $("#data-form #comment").val(response.comment);
 
 
         }
@@ -264,7 +264,7 @@
           url: '<?php echo base_url($controller . "/remove") ?>',
           type: 'post',
           data: {
-           id:id,
+            id: id,
             [csrfToken]: csrfHash
           },
           dataType: 'json',
